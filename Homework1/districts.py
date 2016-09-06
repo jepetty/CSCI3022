2952b8b04044c6b24262a8084f4391ce800a2f7d
jepetty <jessica.petty@colorado.edu>
2016-09-06 12:01:58 -0600

    cleaned up some messy code/comments

--- Homework1/districts.py
+++ Homework1/districts.py
@@ -3,23 +3,6 @@ from csv import DictReader, DictWriter
 import heapq
 
 kHEADER = ["STATE", "DISTRICT", "MARGIN"]
-kALASKA = """LINE,STATE ABBREVIATION,STATE,D,FEC ID#,(I),CANDIDATE NAME (First),CANDIDATE NAME (Last),CANDIDATE NAME,TOTAL VOTES,PARTY,PRIMARY VOTES,PRIMARY %,RUNOFF VOTES,RUNOFF %,GENERAL VOTES ,GENERAL %,GE RUNOFF ELECTION VOTES (LA),GE RUNOFF ELECTION % (LA),"COMBINED GE PARTY TOTALS (CT, NY, SC)","COMBINED % (CT, NY, SC)",GE WINNER INDICATOR,FOOTNOTES
-52,AK,Alaska,0,H6AK00045,(I),Don,Young,"Young, Don",,R,"79,393","74,29%",,,"142,572","50,97%",,,,,W,
-53,AK,Alaska,0,H0AK00097,,John R.,Cox,"Cox, John R.",,R,"14,497","13,57%",,,,,,,,,,
-54,AK,Alaska,0,H4AK00149,,David  ,Seaward,"Seaward, David  ",,R,"7,604","7,12%",,,,,,,,,,
-55,AK,Alaska,0,H4AK00131,,"David,Dohner,"Dohner, David",,R,"5,373","5,03%",,,,,,,,,,
-56,AK,Alaska,0,n/a,,,,,Party Votes:,R,"106,867",,,,,,,,,,,
-57,AK,Alaska,0,H4AK00123,,Forrest ,Dunbar,"Dunbar, Forrest ",,D,"38,735","80,92%",,,"114,602","40,97%",,,,,,
-58,AK,Alaska,0,H4AK00057,,Frank J.,Vondersaar,"Vondersaar, Frank J.",,D,"9,132","19,08%",,,,,,,,,,
-59,AK,Alaska,0,n/a,,,,,Party Votes:,D,"47,867",,,,,,,,,,,
-60,AK,Alaska,0,H2AK00143,,Jim C.,McDermott,"McDermott, Jim C.",,LIB,"13,437","100,00%",,,"21,29","7,61%",,,,,,
-61,AK,Alaska,0,n/a,,,,,Party Votes:,LIB,"13,437",,,,,,,,,,,
-62,AK,Alaska,0,n/a,,,,Scattered,,W,,,,,"1,277","0,46%",,,,,,
-63,AK,Alaska,0,n/a,,,,,District Votes:,,"168,171",,,,"279,741",,,,,,,
-64,AK,Alaska,H,n/a,,,,,Total State Votes:,,"168,171",,,,"279,741",,,,,,,
-65,AK,Alaska,,n/a,,,,,,,,,,,,,,,,,,
-66,AK,Alaska,,n/a,,,,,,,,,,,,,,,,,,
-""".split("\n")
 
 def district_margins(state_lines):
     """
@@ -28,23 +11,6 @@ def district_margins(state_lines):
 
     @lines The csv rows that correspond to the districts of a single state
     """
-    #for x in state_lines:
-        #if x["D"] != "H" and x["STATE"] == "Louisiana":
-            #print("DISTRICT: ", x["D"]," PRIMARY %: ", x["PRIMARY %"], " RUNOFF %: ", x["RUNOFF %"], " GENERAL %: ", x["GENERAL %"], " GE RUNOFF ELECTION % (LA): ",
-                #x["GE RUNOFF ELECTION % (LA)"], " COMBINED % (CT, NY, SC): ", x["COMBINED % (CT, NY, SC)"])
-            #print(x["D"], " ",x["TOTAL VOTES"], " ", x["GENERAL VOTES "])
-            #print(x["D"], " ", float(x["GENERAL %"].replace(",",".").replace("%","")))
-
-    #for x in state_lines:
-    #    if x["D"] and x["D"] != "H":
-    #        print(x["STATE"], " ",x["D"], " ", x["GENERAL %"])
-    #        winning_district(x["D"],state_lines)
-
-    #margins = {}
-    #for ss in set(x["D"] for x in state_lines if x["D"] and x["D"] != "H"):
-    #    margins[ss] = winning_district(ss,state_lines)
-    #return margins
-
     margins = {}
     old_district = -1
     maxper = 0
@@ -83,18 +49,6 @@ def district_margins(state_lines):
     margins[old_district] = maxper - second
     return margins
 
-
-    #margins = {}
-    #for x in state_lines:
-    #    if x["D"] != "H":
-    #        margins[x["D"].replace(" - FULL TERM", "").replace(" - UNEXPIRED TERM","")] = winning_district(x["D"], state_lines)
-    #return margins
-
-    # Complete this function
-    #return dict((int(x["D"].replace(" - FULL TERM", "").replace(" - UNEXPIRED TERM", "")), 
-        #25) for x in state_lines if x["D"] and x["D"] != "H")
-        #winner(x["D"], state_lines)) for x in state_lines if x["D"] and x["D"] != "H")
-
 def all_states(lines):
     """
     Return all of the states (column "STATE") in list created from a
@@ -103,7 +57,6 @@ def all_states(lines):
     """
     return set((x["STATE"]) for x in lines if x["STATE"])
 
-
 def all_state_rows(lines, state):
     """
     Given a list of output from DictReader, filter to the rows from a single state.
@@ -111,32 +64,9 @@ def all_state_rows(lines, state):
     @state Only return lines from this state
     @lines Only return lines from this larger list
     """
-
     for ii in lines:
         if ii["STATE"] == state:
             yield ii
-
-def winning_district(district, state_info):
-    maxper = 0;
-    second = 0;
-    for x in state_info:
-        vote_per = x["GENERAL %"].replace(",",".").replace("%","")
-        print(vote_per)
-        print(x["D"], " ", district)
-        if x["D"] == district and vote_per != "":
-            #print(x["GENERAL %"].replace(",",".").replace("%",""))
-            #votes = x["GENERAL %"].replace(",",".").replace("%","")
-            #votes = float(x["GENERAL %"].replace(",",".").replace("%",""))
-            if float(vote_per) > maxper:
-                second = maxper
-                maxper = float(vote_per)
-            elif float(vote_per) < maxper and float(vote_per) > second:
-                second = float(vote_per)
-    return (maxper - second)
-
-    #if vote_percentage == "":
-        #print(district, " ", vote_percentage)
-    #return vote_percentage["GENERAL %"]
 
 if __name__ == "__main__":
     # You shouldn't need to modify this part of the code
@@ -149,7 +79,6 @@ if __name__ == "__main__":
         margins = district_margins(all_state_rows(lines, state))
 
         for ii in margins:
-            #print(state, " ", ii, " ", margins[ii])
             summary[(state, ii)] = margins[ii]
 
     for ii, mm in sorted(summary.items(), key=lambda x: x[1]):
