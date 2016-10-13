48b4e8aba925e73dba18ef962724ada52e97a768
jepetty <jessica.petty@colorado.edu>
2016-10-13 00:10:48 -0600

    have pre writeup and file complete for ttest

--- Homework3/ttest.py
+++ Homework3/ttest.py
@@ -30,8 +30,7 @@ def t_statistic(mean1, mean2, n1, n2, svar1, svar2):
     Compute the t-statistic for the given estimates
     """
 
-    # Complete this funtion
-    return 0.0
+    return (mean1 - mean2)/sqrt((svar1/n1 + svar2/n2))
 
 def t_test(sample1, sample2):
     """
@@ -41,9 +40,14 @@ def t_test(sample1, sample2):
     @param sample2 An iterable of the second sample
     """
 
-
-
-    return 0.0
+    mean1 = mean(sample1)
+    mean2 = mean(sample2)
+    var1 = unbiased_sample_variance(sample1, mean1)
+    var2 = unbiased_sample_variance(sample2, mean2)
+    tstat = t_statistic(mean1, mean2, len(sample1), len(sample2), var1, var2)
+    dof = degrees_of_freedom(var1, var2, len(sample1), len(sample2))
+    
+    return 2*(1-t.cdf(abs(tstat), dof+1))
 
 if __name__ == "__main__":
     v1 = [5, 7, 5, 3, 5, 3, 3, 9]
